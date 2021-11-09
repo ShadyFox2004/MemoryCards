33de66418e434e851d64348f56fd63fadcfb1881
ShadyFox2004 <amboudreau.amb@gmail.com>
2021-11-09 11:17:09 -0500

    Program is working

--- memory-cards/src/main/java/edu/altaspace/Main.java
+++ memory-cards/src/main/java/edu/altaspace/Main.java
@@ -1,16 +1,17 @@
 package edu.altaspace;
 
-import javax.swing.UIClientPropertyKey;
+import java.util.Random;
 
-public class App 
+public class Main 
 {
     public static void main( String[] args )
     {
         Card[] cards = CardMaker.createCards();
+        Random random = new Random();
 
         while(true){
-            Card myCard = Utility.randomCard(cards);
-            Utility.showOneAspect(myCard);
+            Card myCard = Utility.randomCard(cards, random);
+            Utility.showOneAspect(myCard, random);
             String answer = Utility.askQuestion("?");
             System.out.println(myCard);
             System.out.println("Your answer was: " + answer);
--- memory-cards/src/main/java/edu/altaspace/Utility.java
+++ memory-cards/src/main/java/edu/altaspace/Utility.java
@@ -5,28 +5,29 @@ import java.util.Scanner;
 
 
 public class Utility {
+    private Utility(){}
+
     public static String askQuestion(String question) {
         System.out.print(question + ": ");
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
     }
 
-    public static int headOrTail() {
-        Random  random = new Random();
-        return random.nextInt()%2; 
+    public static int headOrTail(Random random) {
+        return random.nextInt(1); 
     }
 
-    public static void showOneAspect(Card card){
-        if(Utility.headOrTail() == 0){
+    public static void showOneAspect(Card card, Random  random){
+        if(Utility.headOrTail(random) == 0){
             System.out.println("Guess the content of: " + card.getName()+"\n");
         } else {
             System.out.println("Guess witch reference it is:" + card.getText() + "\n");
         }
     }
 
-    public static Card randomCard(Card[] cards){
-        Random  random = new Random();
-        return cards[random.nextInt()%cards.length];
+    public static Card randomCard(Card[] cards, Random  random){
+        int cardNumber = random.nextInt(cards.length);
+        return cards[cardNumber];
     }
 
 }
