d508bfa2aa0f695a923a009a08fb448c740f64c4
ShadyFox2004 <amboudreau.amb@gmail.com>
2021-11-09 10:45:26 -0500

    App is done

--- memory-cards/src/main/java/edu/altaspace/App.java
+++ memory-cards/src/main/java/edu/altaspace/App.java
@@ -1,15 +1,21 @@
 package edu.altaspace;
 
+import javax.swing.UIClientPropertyKey;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        Card[] myCards = CardMaker.createCards();
-        /*
-        for (Card card : myCards) {
-            System.out.println(card);
+        Card[] cards = CardMaker.createCards();
+
+        while(true){
+            Card myCard = Utility.randomCard(cards);
+            Utility.showOneAspect(myCard);
+            String answer = Utility.askQuestion("?");
+            System.out.println(myCard);
+            System.out.println("Your answer was: " + answer);
+            Utility.askQuestion("How close were you?");
+            System.out.println("\n\n\nNext one, get ready!");
         }
-        
-        */
     }
 }
